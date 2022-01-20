dba2b78f87a072e3d043946aee769893e7ff8948
jason-rehatched <jason@rehatched.com>
2022-01-20 08:59:49 -0800

    Update about.blade.php

--- resources/views/public/about.blade.php
+++ resources/views/public/about.blade.php
@@ -43,10 +43,44 @@
                                 <div class="gen-blog-post">
                                     <div class="gen-blog-contain">
                                         <h5 class="gen-blog-title">
-                                            Yup, this is the about page...
+                                            So, about us...
                                         </h5>
                                         <p>
-                                            Coming soon..
+                                            Tired of having your holiday, birthday, or some other special day ruined by
+                                            these low-life, degenerates? Yah, so are we!
+                                        </p>
+                                        <p>
+                                            Unfortunately, not everyone has access to a doorbell camera or some other cloud based camera.
+                                            Even those of us with a camera, still find it difficult to figure out excactly who that thief is!
+                                        </p>
+                                        <p>
+                                            PackageThieves was designed to appeal to a broader audience by allowing more people to video recorded
+                                            video with hope of identifying who that package thief was. In the event you do discover who exactly took
+                                            that package off your doorstep, the decision is up to you.
+                                        </p>
+                                        <ul>
+                                            <li>
+                                                Handle it civilly by speaking with the thief. Confronting them with video evidence of their
+                                                wrongdoing <em><strong>may</strong></em> get your package back.
+                                            </li>
+                                            <li>
+                                                Notify your local police or sheriff department. Even if you or they do not decide to seek criminal
+                                                prosecution, they may still be able to help facilitate getting your package back. Of course, this
+                                                is entirely based on each department's policies and if they have enough time and staffing to
+                                                help in a matter such as this.
+                                            </li>
+                                            <li>
+                                                Yah, I know what you're thinking for this one...probably not the best idea. Just let the scenario
+                                                play out in your head and be satisfied with that. &#128540;
+                                            </li>
+                                        </ul>
+                                        <p>
+                                            In the end, many shippers and online merchants are very understanding and will readily replace your
+                                            package for you.
+                                        </p>
+                                        <p>
+                                            With that, enjoy your stay here, no matter how brief it is, and watch what some people will lower
+                                            themselves to in life! You never know who you may recognize!
                                         </p>
                                     </div>
                                 </div>
