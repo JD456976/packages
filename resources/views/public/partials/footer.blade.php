51d2bed64bdb6bf271a3869e02d73b05abae85d1
jason-rehatched <jason@rehatched.com>
2022-01-19 21:37:40 -0800

    Footer Changes

--- resources/views/public/partials/footer.blade.php
+++ resources/views/public/partials/footer.blade.php
@@ -9,67 +9,53 @@
                                 <div class="col-sm-12">
                                     <img src="images/asset-0.png" class="gen-footer-logo" alt="gen-footer-logo">
                                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-                                    <ul class="social-link">
-                                        <li><a href="#" class="facebook"><i class="fab fa-facebook-f"></i></a></li>
-                                        <li><a href="#" class="facebook"><i class="fab fa-instagram"></i></a></li>
-                                        <li><a href="#" class="facebook"><i class="fab fa-skype"></i></a></li>
-                                        <li><a href="#" class="facebook"><i class="fab fa-twitter"></i></a></li>
-                                    </ul>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div class="col-xl-3 col-md-6">
                         <div class="widget">
-                            <h4 class="footer-title">Explore</h4>
+                            <h4 class="footer-title">Menu</h4>
                             <div class="menu-explore-container">
                                 <ul class="menu">
                                     <li class="menu-item">
-                                        <a href="index.html" aria-current="page">Home</a>
+                                        <a href="{{ route('home') }}" aria-current="page">Home</a>
                                     </li>
-                                    <li class="menu-item"><a href="movies-pagination.html">Movies</a></li>
-                                    <li class="menu-item"><a href="tv-shows-pagination.html">Tv Shows</a></li>
-                                    <li class="menu-item"><a href="video-pagination.html">Videos</a></li>
-                                    <li class="menu-item"><a href="#">Actors</a></li>
-                                    <li class="menu-item"><a href="#">Basketball</a></li>
-                                    <li class="menu-item"><a href="#">Celebrity</a></li>
-                                    <li class="menu-item"><a href="#">Cross</a></li>
+                                    <li class="menu-item">
+                                        <a href="{{ route('about') }}" aria-current="page">About</a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="{{ route('contact') }}" aria-current="page">Contact</a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="{{ route('video.create') }}" aria-current="page">Upload Video</a>
+                                    </li>
                                 </ul>
                             </div>
                         </div>
                     </div>
                     <div class="col-xl-3 col-md-6">
                         <div class="widget">
-                            <h4 class="footer-title">Company</h4>
+                            <h4 class="footer-title">Resources</h4>
                             <div class="menu-about-container">
                                 <ul class="menu">
                                     <li class="menu-item"><a href="contact-us.html">Company</a>
                                     </li>
-                                    <li class="menu-item"><a href="contact-us.html">Privacy
-                                            Policy</a></li>
-                                    <li class="menu-item"><a href="contact-us.html">Terms Of
-                                            Use</a></li>
-                                    <li class="menu-item"><a href="contact-us.html">Help
-                                            Center</a></li>
-                                    <li class="menu-item"><a href="contact-us.html">contact us</a></li>
-                                    <li class="menu-item"><a href="pricing-style-1.html">Subscribe</a></li>
-                                    <li class="menu-item"><a href="#">Our Team</a></li>
-                                    <li class="menu-item"><a href="contact-us.html">Faq</a></li>
                                 </ul>
                             </div>
                         </div>
                     </div>
                     <div class="col-xl-3  col-md-6">
                         <div class="widget">
-                            <h4 class="footer-title">Downlaod App</h4>
+                            <h4 class="footer-title">Our Partners</h4>
                             <div class="row">
                                 <div class="col-sm-12">
-                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-                                    <a href="#">
-                                        <img src="images/asset-35.png" class="gen-playstore-logo" alt="playstore">
+                                    <p>Check out some of the great platforms below</p>
+                                    <a href="https://nextdoor.com/" target="_blank">
+                                        <img src="{{ asset('images/next-door.png') }}" class="gen-playstore-logo" alt="nextdoor">
                                     </a>
-                                    <a href="#">
-                                        <img src="images/asset-36.png" class="gen-appstore-logo" alt="appstore">
+                                    <a href="https://patch.com/" target="_blank">
+                                        <img src="{{ asset('images/patch-logo.png') }}" class="gen-appstore-logo" alt="appstore">
                                     </a>
                                 </div>
                             </div>
@@ -82,8 +68,8 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-12 align-self-center">
-                     <span class="gen-copyright"><a target="_blank" href="#"> Copyright 2021 stremlab All Rights
-                           Reserved.</a></span>
+                     <span class="gen-copyright"><a target="_blank" href="#"> (c) <?php echo date("Y"); ?> PackageThieves | All Rights
+                           Reserved</a></span>
                     </div>
                 </div>
             </div>
