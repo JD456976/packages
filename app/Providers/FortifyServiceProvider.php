be9b4b1cea725122502aa715f6ef2b7db6da12a4
jason-rehatched <96211629+JD456976@users.noreply.github.com>
2022-01-20 23:15:49 -0800

    Redirect Fix

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -80,7 +80,7 @@ class FortifyServiceProvider extends ServiceProvider
         $this->app->instance(LoginResponse::class, new class implements LoginResponse {
             public function toResponse($request)
             {
-                return redirect()->url()->previous();
+                return redirect()->back();
             }
         });
     }
