f2f9c864a3309e3c1a7e5d4ccc65404dd9bf6992
jason-rehatched <96211629+JD456976@users.noreply.github.com>
2022-01-24 13:15:03 -0800

    Changed Comments

--- resources/views/frontend/partials/comment.blade.php
+++ resources/views/frontend/partials/comment.blade.php
@@ -2,28 +2,30 @@
     <div class="row height d-flex justify-content-center align-items-center">
         <div class="col-md-7">
             <div class="card">
-                <div class="p-3">
-                    <h6>Comments</h6>
-                </div>
-                <div class="align-items-center p-3 form-color">
-                    {!! Form::open(['route' => ['comment', $video->id], 'method' => 'post']) !!}
-                    <div class="form-group">
-                        @error('comment')
+                @guest()
+                    <h5 class="text-center pt-4" style="color: black">You must be logged in to comment</h5>
+                @endguest
+                @auth()
+                    <div class="align-items-center p-3 form-color">
+                        {!! Form::open(['route' => ['comment', $video->id], 'method' => 'post']) !!}
+                        <div class="form-group">
+                            @error('comment')
                             <div class="alert alert-danger alert-dismissible mb-2" role="alert">
                                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                                     <span aria-hidden="true">×</span>
                                 </button>
-                            <div class="d-flex align-items-center">
-                                <i class="bx bx-error"></i>
+                                <div class="d-flex align-items-center">
+                                    <i class="bx bx-error"></i>
                                     <span>{{$message}}</span>
+                                </div>
                             </div>
+                            @enderror
+                            {!! Form::text('comment', old('comment'), ['class' => 'form-control', 'placeholder' => 'Add Your Comment']) !!}
+                            {!! Form::submit('Add Comment', ['class' => 'btn btn-primary mt-2']) !!}
                         </div>
-                        @enderror
-                        {!! Form::text('comment', old('comment'), ['class' => 'form-control', 'placeholder' => 'Add Your Comment']) !!}
-                        {!! Form::submit('Add Comment', ['class' => 'btn btn-primary mt-2']) !!}
+                        {!! Form::close() !!}
                     </div>
-                    {!! Form::close() !!}
-                </div>
+                @endauth
                 <div class="mt-2">
                     @foreach($comments as $comment)
                         <div class="d-flex flex-row p-3">
@@ -36,7 +38,7 @@
                                     <small>{{ $comment->created_at->diffForHumans() }}
                                         <a href="" data-toggle="modal" data-target="#reportCommentModal"><i class="fa fa-exclamation-triangle"></i></a></small>
                                 </div>
-                                <p style="color:black" class="text-justify comment-text mb-0 border-bottom">
+                                <p style="color:black;" class="text-justify comment-text mb-0 border-bottom">
                                     {{ $comment->text }}
                                 </p>
                             </div>
