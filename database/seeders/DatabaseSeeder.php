480eec5039bd81f54ec31ac63c9cd964b2e1f07e
jason-rehatched <96211629+JD456976@users.noreply.github.com>
2022-01-27 11:52:23 -0800

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            VideoSeeder::class,
+            //VideoSeeder::class,
         ]);
     }
 }
